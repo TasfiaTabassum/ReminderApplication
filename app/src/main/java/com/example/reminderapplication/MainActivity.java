a27a8ace873fa37d2f35d9bca5e169f02c648847
Tasfia Tabassum <tasfia.tabassum.du@gmail.com>
2022-10-22 01:07:59 +0600

    2nd commit

--- app/src/main/java/com/example/reminderapplication/MainActivity.java
+++ app/src/main/java/com/example/reminderapplication/MainActivity.java
@@ -3,9 +3,17 @@ package com.example.reminderapplication;
 import static com.example.reminderapplication.MyWorker.setNotification;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+import androidx.core.content.res.ResourcesCompat;
 
+import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
@@ -20,12 +28,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
         btn = findViewById(R.id.btn_start);
         createNotificationChannel();
 
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
                 setNotification(view.getContext());
             }
         });
@@ -33,12 +43,15 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void createNotificationChannel() {
-        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
-            NotificationManager notificationManager = getSystemService(NotificationManager.class);
-            notificationManager.createNotificationChannel(new NotificationChannel("noti", "Alerts", NotificationManager.IMPORTANCE_HIGH));
+      //  String id = "Floating notification is working" ;
+
+             if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+           NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+            //NotificationChannel channel = notificationManager.getNotificationChannel(id);
+            notificationManager.createNotificationChannel(new NotificationChannel("notif", "Alerts", NotificationManager.IMPORTANCE_HIGH));
         }
         else{
-            NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+          NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
             notificationManager.notify();
         }
     }
--- app/src/main/java/com/example/reminderapplication/MyWorker.java
+++ app/src/main/java/com/example/reminderapplication/MyWorker.java
@@ -1,8 +1,11 @@
 package com.example.reminderapplication;
 
+import android.accessibilityservice.AccessibilityService;
+import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.core.app.NotificationCompat;
@@ -12,19 +15,24 @@ import androidx.work.WorkManager;
 import androidx.work.Worker;
 import androidx.work.WorkerParameters ;
 
+import java.sql.SQLOutput;
 import java.util.ConcurrentModificationException;
+
 import  java.util.concurrent.TimeUnit ;
 
 
 public class MyWorker extends Worker {
 
+   // private static final int NOTIFICATION_ID = 100 ;
+
     public MyWorker(@NonNull Context context, @NonNull WorkerParameters workerParams) {
         super(context, workerParams);
     }
 
-    private void showNotification(){
-        String description = "";
+    public void showNotification(){
+        String description = "Hello";
         Intent intent = new Intent(getApplicationContext(),MainActivity.class);
+
 
         PendingIntent pendingIntent = PendingIntent.getActivity(getApplicationContext(), 0 , intent, 0);
 
@@ -39,7 +47,8 @@ public class MyWorker extends Worker {
                 .setContentIntent(pendingIntent);
 
         NotificationManagerCompat notificationManagerCompat = NotificationManagerCompat.from(getApplicationContext());
-        notificationManagerCompat.notify(198, builder.build());
+        notificationManagerCompat.notify(1, builder.build());
+
     }
 
 
@@ -47,14 +56,24 @@ public class MyWorker extends Worker {
     @NonNull
     @Override
     public Result doWork() {
-        showNotification();
-        return null;
+
+        try{
+            showNotification();
+            return Result.success();
+        }
+        catch(Exception e){
+            System.out.println(e.getMessage());
+            return  Result.failure();
+        }
+
+        //return null;
     }
 
 
     public static void setNotification(Context context){
-        PeriodicWorkRequest periodicWorkRequest = new PeriodicWorkRequest.Builder(MyWorker.class, 1, TimeUnit.MINUTES)
-                .setInitialDelay(1, TimeUnit.MINUTES) .build();
+        PeriodicWorkRequest periodicWorkRequest = new PeriodicWorkRequest.Builder(MyWorker.class, 1000, TimeUnit.MILLISECONDS)
+                .setInitialDelay(1000, TimeUnit.MILLISECONDS)
+                .build();
         WorkManager.getInstance(context).enqueue(periodicWorkRequest);
     }
 
